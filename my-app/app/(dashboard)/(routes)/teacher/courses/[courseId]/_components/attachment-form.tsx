1f2044963b66aabc0ad78295c06f538d076aa58a
MudasirIrshad <mudasirirshad47@gmail.com>
2025-03-14 22:18:07 +0500

    attachment part done

--- my-app/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ my-app/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -3,22 +3,23 @@ import React, { useState } from "react";
 import * as z from "zod";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
-import { ImageIcon, PencilIcon, PlusCircle } from "lucide-react";
+import { File, Loader2, PencilIcon, PlusCircle, X } from "lucide-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Attachment, Course } from "@prisma/client";
 import { FileUpload } from "@/components/file-upload";
 
 interface AttachmentFormProps {
-  initialData: Course & { attahments: Attachment[] };
+  initialData: Course & { attachments: Attachment[] };
   courseId: string;
 }
 const formSchema = z.object({
-  attachmentUrl: z.string().min(1),
+  url: z.string().min(1),
 });
 function AttachmentForm({ initialData, courseId }: AttachmentFormProps) {
   const router = useRouter();
   const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
   const toggleEdit = () => {
     setIsEditing(!isEditing);
   };
@@ -30,6 +31,19 @@ function AttachmentForm({ initialData, courseId }: AttachmentFormProps) {
       router.refresh();
     } catch (error) {
       toast.error("Something went wrong");
+    }
+  };
+  const onDelete = async (id: string) => {
+    try {
+      console.log("THis is the id", id);
+      setDeletingId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success("Attachement Deleted");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setDeletingId(null);
     }
   };
   return (
@@ -44,18 +58,44 @@ function AttachmentForm({ initialData, courseId }: AttachmentFormProps) {
               Add a file
             </>
           )}
-          {isEditing && initialData.attahments && (
-            <>
-              <PencilIcon className="h-4 w-4" />
-              Edit file
-            </>
-          )}
         </Button>
       </div>
       {!isEditing && (
         <>
-          {initialData.attahments && (
+          {initialData.attachments?.length === 0 && (
             <p className="text-sm mt-2 text-slate-500 italic">No attachments</p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <>
+              <div className="space-y-2">
+                {initialData.attachments.map((attachment) => {
+                  return (
+                    <div
+                      key={attachment.id}
+                      className="flex items-center p-3 w-full bg-sky-100 border-sky-200 text-sky-700 rounded-md"
+                    >
+                      <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                      <p className="text-xs line-clamp-1">{attachment.name}</p>
+                      {deletingId === attachment.id && (
+                        <>
+                          <Loader2 className="h-4 w-4 animate-spin" />
+                        </>
+                      )}
+                      {deletingId !== attachment.id && (
+                        <button
+                          className="ml-auto hover:opacity-75"
+                          onClick={() => {
+                            onDelete(attachment.id);
+                          }}
+                        >
+                          <X className="h-4 w-4 ml-2 text-gray-900" />
+                        </button>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </>
           )}
         </>
       )}
