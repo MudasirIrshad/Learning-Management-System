a43d0039fc2fd29c5bbbadd6f40b5f7c6af14e5a
MudasirIrshad <mudasirirshad47@gmail.com>
2025-03-24 11:11:00 +0500

    formatting data table

--- my-app/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ my-app/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,9 +1,21 @@
 "use client";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
@@ -28,6 +40,14 @@ export const columns: ColumnDef<Course>[] = [
         <ArrowUpDown className="ml-2 h-4 w-4" />
       </Button>
     ),
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price") || "0");
+      const formatted = new Intl.NumberFormat("en-Pk", {
+        style: "currency",
+        currency: "PKR",
+      }).format(price);
+      return <p>{formatted}</p>;
+    },
   },
   {
     accessorKey: "isPublished",
@@ -40,7 +60,16 @@ export const columns: ColumnDef<Course>[] = [
         <ArrowUpDown className="ml-2 h-4 w-4" />
       </Button>
     ),
+    cell: ({ row }) => {
+      const isPublished = row.getValue("isPublished") || false;
+      return (
+        <Badge className={cn("bg-black", isPublished && "bg-blue-500")}>
+          {isPublished ? "Published" : "Draft"}
+        </Badge>
+      );
+    },
   },
+
   {
     accessorKey: "createdAt",
     header: ({ column }) => (
@@ -55,6 +84,30 @@ export const columns: ColumnDef<Course>[] = [
     cell: ({ cell }) => {
       const dateValue = cell.getValue() as Date; // Cast to Date
       return format(dateValue, "dd/MM/yyyy HH:mm:ss");
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-4 w-4 p-0">
+              <span className="sr-only">Open Menu</span>
+              <MoreHorizontal className="h-4 w-4 p-0" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" /> Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
